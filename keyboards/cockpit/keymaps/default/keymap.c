824ddbf18a9cc0c636e4cabdc1205dff2650fbb3
Ömer Tunç <44731889+omertuncwho@users.noreply.github.com>
2024-10-18 22:43:44 +0300

    Update keymap.c

--- keyboards/cockpit/keymaps/default/keymap.c
+++ keyboards/cockpit/keymaps/default/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_QWERTY] = LAYOUT_cockpit(
-    KC_TAB,      TR_Q,  TR_W,  TR_E,  TR_R,  TR_T,                 TR_Y,  TR_U,  TR_IDOT,  TR_O,    TR_P,     KC_BSPC,
+    KC_TAB,      TR_Q,  TR_W,  TR_E,  TR_R,  TR_T,                 TR_Y,  TR_U,  TR_I,  TR_O,    TR_P,     KC_BSPC,
     KC_LCTL,    TR_A,  TR_S,  TR_D,  TR_F,  TR_G,                 TR_H,  TR_J,  TR_K,     TR_L,    TR_SCLN,  TR_QUOT,
     LSFT_CAPS,   TR_Z,  TR_X,  TR_C,  TR_V,  TR_B,                 TR_N,  TR_M,  TR_COMM,  TR_DOT,  TR_SLSH,  RSFT_ENT,
                                                   KC_MPLY,   KC_MUTE,
